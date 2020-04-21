2061037f3da2fa786571ea0ab58ae880d8a64a3b
ganeshpat <ganesh.k@pavo-tusker.com>
2020-04-21 17:54:24 +0530

    added proptypes

---
+++ src/Congrats.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Congrats =  ( props ) => {
+  const { success } = props;
+  if (success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congradulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+}
+
+export default Congrats;
---
+++ src/Guessedwords.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { 
+    findByTestAttr,
+    checkProps
+} from '../test/testUtils';
+import GuessedWords from './Guessedwords';
